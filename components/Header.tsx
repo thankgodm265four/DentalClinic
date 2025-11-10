59941d30286ded21a7511b7ad4bb1e74338fbbbb
thankgodm265four <thankgodm265four@gmail.com>
2025-11-10 10:28:13 -0800

    okay

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 const Logo = () => (
     <img 
-        src="https://i.ibb.co/SDcrgDpp/image.png"
+        src="/logo.png"
         alt="White Emerald Dental Care Logo" 
-        className="h-12 w-auto" 
+        className="h-12 w-auto"
     />
 );
 
